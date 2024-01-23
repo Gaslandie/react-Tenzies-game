b3574cd6a41c06445850a3ab995aa65f3abf8ee6
Gassama <gassma803@gmail.com>
2024-01-23 15:49:57 +0100

    ajout de sons au clic sur les box et ajout au confetti lorsque l'utilisateur a gagné la partie

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div>
         <h1 className="tenzie-titre display-3 fw-bold mb-3">Tenzie</h1>
-        <p className="tenzi-para display-6 bg-"> Cliquez sur les dés affichant des chiffres identiques pour les sélectionner. Pour les autres dés, cliquez sur le bouton 'Lancer' pour générer de nouveaux chiffres. Répétez cette opération jusqu'à ce que tous les dés affichent le même chiffre.</p>
+        <p className="tenzi-para display-6 bg-"> Cliquez sur les dés affichant des chiffres identiques pour les sélectionner. Pour les autres dés, cliquez sur le bouton 'Lancer' pour générer de nouveaux chiffres. Répétez cette opération jusqu'à ce que tous les dés cliqués affichent le même chiffre.</p>
     </div>
   )
 }
